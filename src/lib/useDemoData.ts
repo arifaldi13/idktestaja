cf2ce41d4c26d648512b398147ecbb29f7305b11
Putra, A. R. Kusuma <arifaldi13@gmail.com>
2025-12-03 12:37:44 +0700

    Refactor useDemoData to use useSyncExternalStore

--- src/lib/useDemoData.ts
+++ src/lib/useDemoData.ts
@@ -1,17 +1,9 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { DemoDataSnapshot } from '@/types';
+import { useSyncExternalStore } from 'react';
+import type { DemoDataSnapshot } from '@/types';
 import { getSnapshot, subscribe } from './fakeDb';
 
 export function useDemoData(): DemoDataSnapshot {
-    const [snapshot, setSnapshot] = useState<DemoDataSnapshot>(getSnapshot());
-
-    useEffect(() => {
-        const unsub = subscribe(setSnapshot);
-        return () => unsub();
-    }, []);
-
-    return snapshot;
+  return useSyncExternalStore(subscribe, getSnapshot);
 }
-
